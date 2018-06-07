0dc806c380b3f98c51aa649b0f2b5452c66503c5
skirankumar <KIRAN_HIFI@YAHOO.COM>
2018-06-07 19:13:07 +0530

    Add files via upload

---
+++ myfirstjavacode/src/myfirstjavacode/Hellowworld.java
@@ -0,0 +1,10 @@
+package myfirstjavacode;
+
+ class Hellowworld {
+
+	public static void main(String args []) {
+		
+     System.out.println("helloww=orld");
+	}
+
+}
---
+++ myfirstjavacode/src/myfirstjavacode/kiran.java
@@ -0,0 +1,7 @@
+package myfirstjavacode;
+
+public class kiran {
+	public static void main(String args []) {
+	System.out.println("skiran");
+	}
+}
