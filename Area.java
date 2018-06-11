1fe1645d29aa31fa14ec348f97d7178b6a8641f0
S.kiran kumar <KIRAN_HIFI@YAHOO.COM>
2018-06-11 22:03:43 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+
+public class Area {
+	public static void main(String args[]) {
+		final int pi=(int) 3.14;
+		int r=5;
+		{
+			System.out.println("area of circle");
+			int area ;
+			area = pi*r*r;
+			System.out.println("area is"+ area);
+			
+					
+		}
+	}
+
+}
